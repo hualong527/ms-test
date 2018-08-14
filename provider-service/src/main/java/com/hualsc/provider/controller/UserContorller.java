ad568bdb3beaa7a5be1d93c0f1f1c32120a0ccc2
hualong <hualong@shangyuan.cn>
2018-08-14 22:41:44 +0800

    aaa

--- provider-service/src/main/java/com/hualsc/provider/controller/UserContorller.java
+++ provider-service/src/main/java/com/hualsc/provider/controller/UserContorller.java
@@ -1,6 +1,7 @@
 package com.hualsc.provider.controller;
 
 import com.hualsc.provider.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserContorller {
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("getUser")
     public User getUserInfo(@RequestParam("id") Long id){
         User user = new User();
         user.setId(id);
-        user.setName("test");
+        user.setName("test:"+port);
         return user;
     }
 }
